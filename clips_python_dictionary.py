a44d14b3ff76469ae78bafe743ef7854e48c2517
EduardoAyora <eduardoaayora64@gmail.com>
2021-12-01 22:36:36 -0500

    Devolver isFinished en la respuesta

--- clips_python_dictionary.py
+++ clips_python_dictionary.py
@@ -1,7 +1,10 @@
-clips_python_messages = {
+clips_python_questions = {
     "construir": "¿Le gusta la construcción?",
     "computadoras": "¿Le gustan las computadoras?",
     "geometria": "¿Le gusta la geometría?",
+}
+
+clips_python_results = {
     "computacion": "Su carrera ideal es la ingeniería en computación",
     "civil": "Su carrera ideal es la ingeniería civil",
     "arquitectura": "Su carrera ideal es la arquitectura",
@@ -13,8 +16,13 @@ def convert_clips_mssg_to_response(clips_mssg):
     response = {
         "message": 'No se ha realizado una pregunta'
     }
-    for key in clips_python_messages:
+    for key in clips_python_questions:
         if key in clips_mssg:
-            response['message'] = clips_python_messages[key]
+            response['message'] = clips_python_questions[key]
             response['topic'] = key
-    return response
+            return response
+    for key in clips_python_results:
+        if key in clips_mssg:
+            response['message'] = clips_python_results[key]
+            response['isFinished'] = True
+            return response
